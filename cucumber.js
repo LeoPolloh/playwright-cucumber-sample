d5177a3063ac679ca50de8c73f6701130afd075a
Leopoldo De Matteo <leopoldo.dematteo@alten.it>
2025-04-15 15:42:20 +0200

    change reports format

--- cucumber.js
+++ cucumber.js
@@ -1,3 +1,3 @@
 module.exports = {
-  default: `--require-module ts-node/register --require ./src/support/**/*.ts --require ./src/steps/**/*.ts --format json:./reports/cucumber_report.json --publish-quiet`
+  default: `--require-module ts-node/register --require ./src/support/**/*.ts --require ./src/steps/**/*.ts --format html:./reports/cucumber_report.html`
 };
